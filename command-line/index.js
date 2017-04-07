a2c90ab23a8026dc932dd13d9b1182b1a0395539
Pedro Lucas <pedro@objota.com.br>
2017-04-07 16:37:03 -0300

    Adicionado estrutura básica para o material do workshop

---
+++ command-line/index.js
@@ -0,0 +1,28 @@
+// Importo as bibliotecas que vamos utilizar
+const program = require('commander');
+const ProgressBar = require('progress');
+
+const bar = new ProgressBar('Imprimindo [:bar] :current/:total :token1', {
+  total: 100
+});;
+
+program
+.version('0.0.1')
+.option('-p, --print', 'Imprimir alguma coisa na tela')
+.parse(process.argv);
+
+if (program.print) {
+
+  // Uso a função setInterval para executar até chegar no final da barra de progresso
+  const timerRef = setInterval(() => {
+    bar.tick({
+      token1: "Passou " + bar.curr + " vezes no timer"
+    });
+    if (bar.complete) {
+      clearInterval(timerRef);
+    }else if (bar.curr === 5) {
+      bar.interrupt('Essa mensagem aparece quando o valor atual for 5');
+    }
+  }, 100);
+
+}
