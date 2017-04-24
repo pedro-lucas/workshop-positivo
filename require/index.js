c042b2f59cf7276d06c2a7f02df4ef59a4e5acb4
Pedro Lucas <pedro@objota.com.br>
2017-04-24 09:10:10 -0300

    Adicionado comentários ao exemplo

--- require/index.js
+++ require/index.js
@@ -1,6 +1,9 @@
-const lib = require('./lib');
+const lib = require('./lib'); //Arquivo criado para o projeto
+const colors = require('colors'); //Módulo adicionado as dependências
+const path = require('path'); //Módulo nativo do Node.js
+
 const TheClass = lib.TheClass;
 const nClass = new TheClass();
 
-console.log('lib sum', lib.sum(1, 1));
-console.log('TheClass property value', `"${nClass.value}"`);
+console.log('lib sum'.blue, ('"'+lib.sum(1, 1)+'"').green);
+console.log('TheClass property value'.blue, `"${nClass.value}"`.green);
